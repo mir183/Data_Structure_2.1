020b4c159c237d781c4e250bd66102ea885dc808
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-10-29 22:22:03 +0600

    Add files via upload

---
+++ Recursion/Factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    int number, result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    result = factorial(number);
+
+    printf("Factorial of %d is: %d", number, result);
+    return 0;
+}
